4ba3b4cbbe6e9651ff234d4dbab7dca6d92c2381
Salma Hefnawy <swalid@aucegypt.edu>
2019-07-08 08:18:43 +0200

    auth trial

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -17,3 +17,16 @@ exports.createTicket = (req, res, next) => {
       console.log(err);
     });
 };
+
+exports.getTickets = (req, res, next) => {
+  Ticket.findAll()
+    .then(tickets => {
+      res.status(200).json({
+        tickets
+      });
+    })
+    .catch(err => {
+      res.status(400).json({ message: "loading tickets failed" });
+      console.log(err);
+    });
+};
--- routes/ticket.js
+++ routes/ticket.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const ticketController = require("../controllers/ticketController");
 
 router.post("/post-ticket", ticketController.createTicket);
+router.get("/get-tickets", ticketController.getTickets);
 
 module.exports = router;
