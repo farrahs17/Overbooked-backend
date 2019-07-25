20579875345d9a28dd3c4c03d35ace8f6575cf11
farrahs17 <farrah.shawky.95@gmail.com>
2019-07-25 19:04:36 +0200

    added stripe (non functional)

---
+++ controllers/checkoutController.js
@@ -0,0 +1,14 @@
+const stripe = require("stripe")("sk_test_fZdwtyvNiGY69eNm9fsluWXi00dYJW06sO");
+
+// Token is created using Checkout or Elements!
+// Get the payment token ID submitted by the form:
+const token = request.body.stripeToken; // Using Express
+
+(async () => {
+  const charge = await stripe.charges.create({
+    amount: 999,
+    currency: "usd",
+    description: "Example charge",
+    source: token
+  });
+})();
